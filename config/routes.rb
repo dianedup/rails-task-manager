bb7cf0b71c3c9747965ec3b915ea2d2f9e4613b7
Diane du Payrat <dianedupayrat@gmail.com>
2019-03-12 13:15:59 +0100

    List tasks and View details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ALL TASKS
+  get 'tasks', to: 'tasks#index'
+
+  # READ
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
 end
